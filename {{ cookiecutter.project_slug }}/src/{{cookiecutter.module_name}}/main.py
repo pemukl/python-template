3829730357ce975027cada817c5bba5d2a266524
Marc Schneider <marc@shnei.de>
2024-09-22 11:23:44 +0200

    add env variables to flow

--- {{ cookiecutter.project_slug }}/src/{{cookiecutter.module_name}}/main.py
+++ {{ cookiecutter.project_slug }}/src/{{cookiecutter.module_name}}/main.py
@@ -1,15 +1,16 @@
 import os
 
-from {{cookiecutter.module_name}}.util import logger
+from {{cookiecutter.module_name}}.util import config, logger
 
 
 def histring():
-    return f"Your {{cookiecutter.project_name}} journey starts here :) (on {os.getenv('ENVIRONMENT')})"
+    return f"Your {{cookiecutter.project_name}} journey starts here :) (on {config.env.name})"
 
 
 def main():
     # TODO your journey starts here
     logger.info(histring())
+    logger.error(f"I have a secret env variable for you: {os.getenv('SECRET_KEY')}")
 
 
 if __name__ == "__main__":
--- {{ cookiecutter.project_slug }}/src/{{cookiecutter.module_name}}/util.py
+++ {{ cookiecutter.project_slug }}/src/{{cookiecutter.module_name}}/util.py
@@ -1,18 +1,54 @@
-import logging
 import os
+import sys
 from pathlib import Path
 from typing import Any, Dict, Union
 
 import yaml
 from dotenv import load_dotenv
+from loguru import logger
+
+"""
+This module contains utility functions for the project. It exports:
+
+- `ROOT_DIR`: the root directory of the project
+- `DATA_DIR`: the data directory of the project
+- `logger`: the logger for the project
+- `config`: dictionary containing the config for the project. Config is loaded from `config.yaml` in the `config` directory.
+"""
+
 
 load_dotenv()
-
-logger = logging.getLogger('{{cookiecutter.module_name}}')
 
 
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir)
 
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+default_format = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+    "<level>{message}</level>"
+)
+
+custom_format = (
+    "<green>{time:HH:mm:ss}</green> <cyan>{function}</cyan>:<cyan>{line}</cyan> | "
+    "<level>{message}</level>"
+)
+logger.remove()
+logger.add(sys.stderr, format=custom_format)
+
+class Dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, value in self.items():
+            if isinstance(value, dict):
+                self[key] = Dotdict(value)
 
 def load_config(config_file: Union[str, Path]) -> Dict[str, Any]:
     """
@@ -22,56 +58,7 @@ def load_config(config_file: Union[str, Path]) -> Dict[str, Any]:
     :return: the parsed config as dictionary
     """
     with open(os.path.join(ROOT_DIR, "config", config_file)) as fp:
-        return yaml.safe_load(fp)
+        conf = yaml.safe_load(fp)
+        return Dotdict(conf)
 
 config = load_config("config.yaml")
-
-
-def logging_setup(config: Dict):
-    """
-    setup logging based on the configuration
-
-    :param config: the parsed config tree
-    """
-
-    log_conf = config['logging']
-    fmt = log_conf['format']
-
-    class CustomFormatter(logging.Formatter):
-        grey = "\x1b[38;20m"
-        green = "\x1b[32m"
-        yellow = "\x1b[33;20m"
-        red = "\x1b[31;20m"
-        bold_red = "\x1b[31;1m"
-        reset = "\x1b[0m"
-
-        FORMATS = {
-            logging.DEBUG: grey + fmt + reset,
-            logging.INFO: green + fmt + reset,
-            logging.WARNING: yellow + fmt + reset,
-            logging.ERROR: red + fmt + reset,
-            logging.CRITICAL: bold_red + fmt + reset
-        }
-
-        def format(self, record):
-            log_fmt = self.FORMATS.get(record.levelno)
-            formatter = logging.Formatter(log_fmt)
-            formatter.datefmt = log_conf["datefmt"]
-            return formatter.format(record)
-
-    if log_conf['enabled']:
-        level = logging._nameToLevel[log_conf['level'].upper()]
-    else:
-        level = logging.NOTSET
-
-    logger.setLevel(level=level)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(level=level)
-
-    ch.setFormatter(CustomFormatter())
-
-    logger.addHandler(ch)
-
-
-logging_setup(config)
