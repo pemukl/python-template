3a71527565546dba666cd6b58bde2075da55eed2
Marc Schneider <marc@shnei.de>
2024-09-22 11:40:14 +0200

    Commit initial files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     subprocess.call(['pixi', 'install'])
     subprocess.call(['git','init'])
     subprocess.call(['git', 'add', "."])
-    subprocess.call(['git', 'commit', "-m", "Initial commit from python template."])
+    subprocess.call(['git', 'commit', "-m", "Initial commit from python template at https://github.com/pemukl/python-template"])
